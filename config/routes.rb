bd254c32c02fd546ed7ecb5123e79c5d89f77403
KimJinsu66 <jinsu6688@gmail.com>
2021-06-21 00:33:02 +0900

    가계부 routes파일 수정

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :deposite_and_withdrawal_categories
-  resources :user_household_ledgers
+  root to: 'homes#index'
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :household_ledgers, controller: 'user_household_ledgers'
+  namespace :household_ledgers, controller: 'user_household_ledgers' do
+    resources :withdrawals
+    resources :deposites
+  end
+  resources :deposite_and_withdrawal_categories
 end
